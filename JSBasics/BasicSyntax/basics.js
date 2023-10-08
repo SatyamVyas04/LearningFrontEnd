302c6fdeae9d166b7af3122f86bd047d7e39583c
SatyamVyas <vyas.satyam.04@gmail.com>
2023-10-08 20:45:58 +0530

    Basics covered

---
+++ JSBasics/BasicSyntax/basics.js
@@ -0,0 +1,47 @@
+// Majority Basics are just like Python
+// Differences between the Two
+    // Variable Assignment is done using let;
+    // Constant Assignment is done using const;
+    //
+    // DataTypes
+        // Line above or below signifies primitive datatype
+        //
+        //         -------------                             ----
+        // Python: str int float complex list tuple set dict bool None 
+        // JavaScript: String Number BigInt Boolean Symbol undefined null Object
+        //             --------------------------------------------------
+        //
+    // Complex DataStructures
+        // Python has many, Js has 'None', just arrays
+        //
+    // Operator changes
+        // (//) --> Math.floor()
+        // == --> ===
+        // and --> &&
+        // or --> ||
+        // not --> !
+        // type() --> typeof()
+        // for i in list --> for (<let|const> elem of list)
+        // for (let elem in object) prints properties of obj in js
+        // def --> function 
+        // args in necessary --> args given undefined if not given
+        // __init__ --> constructor
+        // self --> this
+        // Constructors Diffs
+            // Python
+                // def __init__(self, radius):
+                //     self.radius = radius
+            // JS
+                // constructor(radius) {
+                //     this.radius = radius;
+                // }
+        // Direct Creation of Object --> 'new' keyword
+
+    // JS Familiarity to Others
+        // Loops: like C, Cpp, Java
+        // Dynamic: like Python
+        // Arrays: like Python
+        // Classes and 'init': like Java
+        // Object creation: like Java ofc
+
+// Array Methods Start
