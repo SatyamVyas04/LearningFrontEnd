fe75b34b34798dc0d529f71ee0c72ac0cffd4c19
SatyamVyas <vyas.satyam.04@gmail.com>
2023-10-15 19:17:39 +0530

    Completed Array methods

--- JSBasics/BasicSyntax/array.js
+++ JSBasics/BasicSyntax/array.js
@@ -14,3 +14,60 @@
 // unshift()
 // valueOf()
 
+const arr = ["1", 2, 'three', true, 1.5, 10];
+console.log(arr);
+
+arr[0] = 'Satyam';
+console.log(arr);
+console.log(arr.length);
+console.log(arr[3]);
+
+arr.push("Hello");
+console.log(arr);
+
+let returnValue = arr.pop();
+console.log(returnValue);
+console.log(arr);
+
+returnValue = arr.unshift("Hello");
+console.log(returnValue);
+console.log(arr);
+// Unshift and Push return new Length
+
+returnValue = arr.splice(1, 3);
+console.log(returnValue); // Removed Portion
+console.log(arr);
+
+returnValue = arr.splice(1, 3, 'Satyam', 'Vyas', '10');
+console.log(returnValue); // Replaced Portion
+console.log(arr);
+
+returnValue = arr.splice(1, 0, ...returnValue); // ... == Spread Operator
+console.log(returnValue); // Nothing returned with inserted
+console.log(arr);
+
+const newarr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+console.log(newarr.slice(2));
+console.log(newarr.slice(2, 5));
+
+newarr.reverse();
+console.log(newarr);
+
+let str = newarr.join();
+console.log(str);
+str = newarr.join(' ');
+console.log(str);
+
+let final = arr.concat(newarr);
+console.log(final);
+
+final = newarr.concat(arr);
+console.log(final);
+
+final = [...newarr, ...arr];
+console.log(final);
+
+final = [newarr, arr];
+console.log(final);
+
+console.log(final[0][7].toUpperCase() + ' & ' + final[1][4]); // 2D Array Traversal
